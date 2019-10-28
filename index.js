d224c3a0e87ae1821cdb455b6afaba2f00c64384
idmyn <hello@davidmyno.rs>
2019-10-28 11:15:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your solution here!
+const pets = ['Milo', 'Otis', 'Garfield']
+
+const append = [...pets.slice(), 'Odie'],
+      prepend = ['Odie', ...pets.slice()],
+      removeLast = [...pets.slice(0, -1)],
+      removeFirst = [...pets.slice(1)]
